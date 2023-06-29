01971919bfee215c04834522b83f8c36b81432fb
aarnold1620 <alexarnold2024@u.northwestern.edu>
2023-06-28 19:42:07 -0500

    image dividers and blog update

---
+++ Arnold/snowclassifier/daynight.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+values = []
+for root, dirs, files in os.walk(".\images\W014"):
+    for file in files:
+        path = os.path.join(root, file)
+        
+        image = cv.imread(path)
+        image = cv.cvtColor(image, cv.COLOR_BGR2GRAY )
+        
+        values.append(np.average(image))
+        if np.average(image) <90 and np.average(image) > 70:
+            cv.imwrite('image.jpeg', cv.imread(path))
+        
+print('plotting')
+counts, bins, patches = plt.hist(values)
+for count, patch in zip(counts, patches):
+    plt.annotate(str(int(count)), xy=(patch.get_x() + patch.get_width() / 2, patch.get_height()),
+                 xytext=(0, 5), textcoords='offset points', ha='center', va='bottom')
+plt.title('Brightness')
+
+# Display the chart
+plt.show()
+    

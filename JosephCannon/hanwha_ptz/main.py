e227f6fb58fb31745075d174baccfe0c503a7c6c
JosephCanMan <135178621+JosephCanMan@users.noreply.github.com>
2023-06-30 10:16:00 -0500

    Update main.py

--- JosephCannon/hanwha_ptz/main.py
+++ JosephCannon/hanwha_ptz/main.py
@@ -1,7 +1,7 @@
 from source import main_to_update
 
 
-Camera1 = main_to_update.CameraControl('10.31.81.11', 'admin', 'why1not#')
+Camera1 = main_to_update.CameraControl('10.31.81.11', 'admin', '')
 
 # Camera1.relative_control(80, 30)
 # Camera1.continuous_control(5)

e3ded2331e44021b03bd26a8125fda564743dc13
FranciscoLozCoding <francisco.lozano33@outlook.com>
2023-08-09 15:37:16 +0000

    First commit of the containerization for the PTZ sampler

--- JosephCannon/hanwha_ptz/main.py
+++ JosephCannon/hanwha_ptz/main.py
@@ -1,10 +1,63 @@
+#import sys
+#sys.path.append("/app/source")
+
+import time
+
+import numpy as np
+
 from source import sunapi_control
 from source import sunapi_config
-from source import ubnt_edgeswitch
+#from source import ubnt_edgeswitch
 
-Camera1 = sunapi_control.CameraControl('10.31.81.11', 'admin', '')
-Camera2 = sunapi_config.CameraConfiguration('10.31.81.11', 'admin', '')
-Switch1 = ubnt_edgeswitch.SwitchInformation('10.31.81.2', 'ubnt', '')
+def main():
+
+    def check_integrity(pos_a, ptz_com, pos_b):
+        POS_a = np.array(pos_a)
+        POS_b = np.array(pos_b)
+        Real_command = np.array(ptz_com)
+        Computed_command = POS_b-POS_a
+        error = np.abs(Real_command-Computed_command)
+        error[0] = error[0]%360
+        print('error: ', error)
+        #assert(np.all(a+c==b))
+
+    number_of_commands = 50
+    Camera1 = sunapi_control.CameraControl('10.31.81.17', 'dario', 'Why1Not@')
+    Camera1.absolute_control(1, 1, 1)
+
+    pan_modulation = 2
+    tilt_modulation = 2
+    zoom_modulation = 1.5
+
+    pan_values = np.array([-5,-1,-0.1,0,0.1,1,5])
+    pan_values = pan_values*pan_modulation
+    tilt_values = np.array([-5,-1,-0.1,0,0.1,1,5])
+    tilt_values = tilt_values*tilt_modulation
+    zoom_values = np.array([-0.3,-0.1,0,0.1,0.3])
+    zoom_values = zoom_values*zoom_modulation
+    
+    PAN=np.random.choice(pan_values, number_of_commands)
+    TILT=np.random.choice(tilt_values, number_of_commands)
+    ZOOM=np.random.choice(zoom_values, number_of_commands)
+    first_position = Camera1.requesting_cameras_position_information()
+    for (pan, tilt, zoom) in zip(PAN, TILT, ZOOM):
+        Camera1.relative_control(pan=pan, tilt=tilt, zoom=zoom)
+        second_position = Camera1.requesting_cameras_position_information()
+        check_integrity(first_position, (pan, tilt, zoom), second_position)
+        #time.sleep(1.0)
+        Camera1.snap_shot('Image.jpg')
+        #time.sleep(1.0)
+        first_position = second_position
+
+    Camera1.absolute_control(1, 1, 1)
+    print('DONE!')
+
+if __name__ == "__main__":
+    main()
+
+#Camera1 = sunapi_control.CameraControl('10.31.81.11', 'admin', '')
+#Camera2 = sunapi_config.CameraConfiguration('10.31.81.11', 'admin', '')
+#Switch1 = ubnt_edgeswitch.SwitchInformation('10.31.81.2', 'ubnt', '')
 
 # Camera1.relative_control(pan=180)
 # Camera1.continuous_control(normalized_speed=True, pan=40)

7fbaf5ff22b69be1f574dfd7456a25e8c8619883
Dario Dematties <ddematties@anl.gov>
2023-08-11 14:42:39 +0000

    Images publication using pywaggle

--- JosephCannon/hanwha_ptz/main.py
+++ JosephCannon/hanwha_ptz/main.py
@@ -3,6 +3,12 @@
 
 import time
 import datetime
+import os
+import glob
+import os.path
+import traceback
+import subprocess
+
 
 import numpy as np
 
@@ -10,19 +16,49 @@ from source import sunapi_control
 from source import sunapi_config
 #from source import ubnt_edgeswitch
 
+from waggle.plugin import Plugin
 
 def set_random_position(camera):
     pan_pos=np.random.randint(0,360)
     tilt_pos=np.random.randint(-20,90)
-    zoom_pos=np.random.randint(0,10)
+    zoom_pos=np.random.randint(0,2)
     camera.absolute_control(float(pan_pos), float(tilt_pos), float(zoom_pos))
 
 def grab_image(camera):
     position = camera.requesting_cameras_position_information()
     pos_str = str(position[0])+','+str(position[1])+','+str(position[2])+' '
     # ct stores current time
-    ct = str(datetime.datetime.now()) 
-    camera.snap_shot(pos_str + ct + '.jpg')
+    ct = str(datetime.datetime.now())
+    camera.snap_shot('/imgs/' + pos_str + ct + '.jpg')
+
+
+def tar_images(output_filename, folder_to_archive):
+    try:
+        cmd = ['tar', 'cvf', output_filename, folder_to_archive]
+        output = subprocess.check_output(cmd).decode("utf-8").strip() 
+        print(output)          
+    except Exception:
+        print(f"E: {traceback.format_exc()}")
+
+
+def publish_images():
+    # run tar -cvf images.tar /imgs
+    tar_images('images.tar', '/imgs')
+    files = glob.glob('/imgs/*.jpg', recursive=True)
+    for f in files:
+        try:
+            os.remove(f)
+        except OSError as e:
+            print("Error: %s : %s" % (f, e.strerror))
+
+    with Plugin() as plugin:
+        ct = str(datetime.datetime.now())
+        os.rename('images.tar', ct+'_images.tar')
+        plugin.upload_file(ct+'_images.tar')
+
+
+
+
 
 def main():
     number_of_commands = 10
@@ -37,7 +73,7 @@ def main():
     pan_values = pan_values*pan_modulation
     tilt_values = np.array([-5,-1,-0.1,0,0.1,1,5])
     tilt_values = tilt_values*tilt_modulation
-    zoom_values = np.array([-0.3,-0.1,0,0.1,0.3])
+    zoom_values = np.array([-0.2,-0.1,0,0.1,0.2])
     zoom_values = zoom_values*zoom_modulation
     
     PAN=np.random.choice(pan_values, number_of_commands)
@@ -49,12 +85,14 @@ def main():
     for (pan, tilt, zoom) in zip(PAN, TILT, ZOOM):
         Camera1.relative_control(pan=pan, tilt=tilt, zoom=zoom)
         grab_image(camera=Camera1)
-        if np.random.rand() > 0.5:
+        if np.random.rand() > 0.85:
             set_random_position(camera=Camera1)
             grab_image(camera=Camera1)
 
 
     Camera1.absolute_control(1, 1, 1)
+    publish_images()
+
     print('DONE!')
 
 if __name__ == "__main__":
